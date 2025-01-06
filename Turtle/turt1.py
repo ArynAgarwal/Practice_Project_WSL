3a5a48da0a4b51841d04140913ed29fc8bb7e115
Aryan Agarwal <aryanagarwal1509@gmail.com>
2025-01-06 10:25:50 +0000

    added a turtle folder and turt.py file inside it

---
+++ Turtle/turt1.py
@@ -0,0 +1,20 @@
+import turtle
+wn=turtle.Screen()
+wn.bgcolor("red")
+aryan=turtle.Turtle()
+aryan.color("white")
+aryan.pensize(3)
+aryan.forward(150)
+aryan.left(90)
+aryan.forward(75)
+aryan.left(90)
+aryan.forward(150)
+aryan.left(90)
+aryan.forward(75)
+aryan.salary=50000
+print(aryan.salary)
+
+wn.exitonclick()
+
+
+
